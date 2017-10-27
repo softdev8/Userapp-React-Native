21182f2de03beaf5b85a355a83389f48268a1cc4
mobiledev418 <oncemoreforyou@outlook.com>
2017-10-27 07:47:20 +0300

    update checkrate nav

--- src/modules/screen/navigator/AppNavigator.js
+++ src/modules/screen/navigator/AppNavigator.js
@@ -4,7 +4,7 @@ import { StackNavigator } from 'react-navigation'
 import LoginScreen from '../login/LoginScreen'
 import HomeScreen from '../home/HomeScreen'
 import CreateOrderScreen from '../createorder/CreateOrderScreen'
-// import CheckRatesScreen from '../checkrates/CheckRatesScreen'
+import CheckRatesScreen from '../checkrates/CheckRatesScreen'
 // import CheckRatesDetailScreen from '../checkrates/CheckRatesDetailScreen'
 // import ForgotPasswordScreen from '../forgotpassword/ForgotPasswordScreen'
 
@@ -14,8 +14,8 @@ import Screens from './Screens'
 const config = {
     [Screens.LOGIN]: { screen: LoginScreen },
     [Screens.HOME]: { screen: HomeScreen },
-    [Screens.CREATE_ORDER]: { screen: CreateOrderScreen }
-    // [Screens.CHECK_RATES]: { screen: CheckRatesScreen },
+    [Screens.CREATE_ORDER]: { screen: CreateOrderScreen },
+    [Screens.CHECK_RATES]: { screen: CheckRatesScreen }
     // [Screens.CHECK_RATES_DETAIL]: { screen: CheckRatesDetailScreen },
     // [Screens.FORGOT_PASSWORD]: { screen: ForgotPasswordScreen }
 }

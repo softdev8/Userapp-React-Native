9c4f2d5e24ddff1db812891d97900bd88953f3e5
MobileDev418 <oncemoreforyou@outlook.com>
2017-10-31 08:24:58 +0200

    update rate reducer

--- src/modules/reducers.js
+++ src/modules/reducers.js
@@ -1,7 +1,7 @@
 import { reducer as nav } from './screen/navigator'
 import homeReducer from './services/home/HomeServices'
 import authReducer from './services/auth/AuthService'
-// import rate from './services/checkrates/checkratesService'
+import rate from './services/checkrates/checkratesService'
 // import modal from './services/modal'
 // import forgotPassword from './services/forgotpassword/ForgotPasswordService'
 // import places from './services/searchplaces/GooglePlacesSearchService'
@@ -12,7 +12,7 @@ export default combineReducers({
     nav,
     homeReducer,
     authReducer,
-    // rate,   
+    rate,   
     // modal,
     // forgotPassword,
     // places,

674207982f4ab1967ad5a9ee2c3befe3ee43fbcd
MobileDev418 <oncemoreforyou@outlook.com>
2017-11-03 20:08:28 +0200

    update

--- src/modules/screen/navigator/Screens.js
+++ src/modules/screen/navigator/Screens.js
@@ -1,7 +1,7 @@
 const Screens = {
     LOGIN: 'Screen/Login',
     HOME: 'Screen/Home',
-    // FORGOT_PASSWORD: 'Screen/ForgotPassword',
+    FORGOT_PASSWORD: 'Screen/ForgotPassword',
     CREATE_ORDER: 'Screen/CreateOrder'
     // CHECK_RATES: 'Screen/CheckRates',
     // CHECK_RATES_DETAIL: 'Screen/CheckRatesDetail'

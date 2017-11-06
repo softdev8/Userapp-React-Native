553fe9ad8e868e3bef5d6e720b164316ca764e22
MobileDev418 <oncemoreforyou@outlook.com>
2017-11-06 16:25:46 +0200

    update screen navigator

--- src/modules/screen/navigator/Screens.js
+++ src/modules/screen/navigator/Screens.js
@@ -2,8 +2,8 @@ const Screens = {
     LOGIN: 'Screen/Login',
     HOME: 'Screen/Home',
     FORGOT_PASSWORD: 'Screen/ForgotPassword',
-    CREATE_ORDER: 'Screen/CreateOrder'
-    // CHECK_RATES: 'Screen/CheckRates',
-    // CHECK_RATES_DETAIL: 'Screen/CheckRatesDetail'
+    CREATE_ORDER: 'Screen/CreateOrder',
+    CHECK_RATES: 'Screen/CheckRates',
+    CHECK_RATES_DETAIL: 'Screen/CheckRatesDetail'
 }
 export default Screens

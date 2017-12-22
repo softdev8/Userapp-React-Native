a2cf93db3cffcd1bf8f41d510ee1e98ac5af2038
MobileDev418 <oncemoreforyou@outlook.com>
2017-12-22 22:54:35 +0200

    fixed checkrate index counter

--- src/modules/services/checkrates/checkratesService.js
+++ src/modules/services/checkrates/checkratesService.js
@@ -17,12 +17,12 @@ export default function reducer(state = initialState, action = {}) {
         case INCREMENT:
             return {
               ...state,
-              count: state.count < 17 ? state.count + 1 : state.count
+              count: state.count < 20 ? state.count + 1 : state.count
             }
         case DECREMENT:
             return {
               ...state,
-              count: state.count > 10 ? state.count - 1 : state.count
+              count: state.count > 0 ? state.count - 1 : state.count
             }
         case FROMADDRESS:
             return {

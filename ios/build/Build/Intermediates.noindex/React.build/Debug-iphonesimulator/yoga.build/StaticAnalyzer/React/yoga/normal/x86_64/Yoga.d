54d97f4890aa4c90dcb756a76b68f60282afa14f
MobileDev418 <oncemoreforyou@outlook.com>
2017-12-22 23:06:10 +0200

    update project

--- ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/yoga.build/StaticAnalyzer/React/yoga/normal/x86_64/Yoga.d
+++ ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/yoga.build/StaticAnalyzer/React/yoga/normal/x86_64/Yoga.d
@@ -1,2 +1,2 @@
 dependencies: \
-  /Volumes/DATA/GitWork/React/userapp-React-Native/node_modules/react-native/ReactCommon/yoga/yoga/Yoga.c
+  /Volumes/DATA/GitWork/React/Userapp-React-Native/node_modules/react-native/ReactCommon/yoga/yoga/Yoga.c
